14594f68a1b5eaa6579f7949f31eaa3bc2d7aaac
Axel Hodler <derxor@gmail.com>
2016-12-19 21:25:39 +0100

    server can be started and stopped

--- src/main/java/co/hodler/DunklesHefe.java
+++ src/main/java/co/hodler/DunklesHefe.java
@@ -1,11 +1,13 @@
 package co.hodler;
 
-import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class DunklesHefe {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws Exception {
     SocketServer s = new SocketServer();
     s.start(8042);
+    TimeUnit.SECONDS.sleep(5);
+    s.stop();
   }
 }
--- src/main/java/co/hodler/SocketServer.java
+++ src/main/java/co/hodler/SocketServer.java
@@ -4,19 +4,34 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class SocketServer {
-  public void start(int port) throws IOException {
-    ServerSocket serverSocket = new ServerSocket(port);
+  private boolean running = true;
+  private ServerSocket serverSocket;
+  private ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-    while (true) {
-      try (
-        Socket clientSocket = serverSocket.accept();
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
-      ) {
-        out.println("HTTP/1.1 200 OK\r\n");
-        out.println("<html><body><p>Hello World</p></body></html>");
+  public void start(int port) throws IOException {
+    serverSocket = new ServerSocket(port);
+
+    executorService.execute(() -> {
+      while (running) {
+        try (
+          Socket clientSocket = serverSocket.accept();
+          PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+        ) {
+          out.println("HTTP/1.1 200 OK\r\n");
+          out.println("<html><body><p>Hello World</p></body></html>");
+        } catch (IOException e) {
+        }
       }
-    }
+    });
+  }
+
+  public void stop() throws Exception {
+    this.running = false;
+    serverSocket.close();
+    executorService.shutdownNow();
   }
 }
