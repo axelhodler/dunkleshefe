abbdc207b2c72c7258a50da7dde7893f39050290
Axel Hodler <derxor@gmail.com>
2016-12-23 08:33:36 +0100

    simple concept of a view

--- src/main/java/co/hodler/DunklesHefe.java
+++ src/main/java/co/hodler/DunklesHefe.java
@@ -5,7 +5,7 @@ import java.util.concurrent.TimeUnit;
 public class DunklesHefe {
 
   public static void main(String[] args) throws Exception {
-    SocketServer s = new SocketServer("");
+    SocketServer s = new SocketServer(new View().toHTML());
     s.start(8042);
     TimeUnit.MINUTES.sleep(5);
     s.stop();
---
+++ src/main/java/co/hodler/View.java
@@ -0,0 +1,7 @@
+package co.hodler;
+
+public class View {
+  public String toHTML() {
+    return "<html><body><p>Hello World</p></body></html>";
+  }
+}
