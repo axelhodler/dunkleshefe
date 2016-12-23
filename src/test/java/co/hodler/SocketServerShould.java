f00d4b58c1a08ec1d93a4de9962be3398d97af6a
Axel Hodler <derxor@gmail.com>
2016-12-23 08:30:20 +0100

    use dynamic content length

--- src/test/java/co/hodler/SocketServerShould.java
+++ src/test/java/co/hodler/SocketServerShould.java
@@ -1,10 +1,10 @@
 package co.hodler;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -17,14 +17,14 @@ public class SocketServerShould {
 
   private SocketServer socketServer;
 
-  @BeforeEach
-  void setUp() {
-    String content = "<html><body><p>Hello World</p></body></html>";
-    socketServer = new SocketServer(content);
+  @AfterEach
+  void cleanUp() throws Exception {
+    socketServer.stop();
   }
 
   @Test
   void free_the_port_when_stopping() throws Exception {
+    socketServer = new SocketServer("");
     socketServer.start(4444);
     socketServer.stop();
     socketServer.start(4444);
@@ -32,19 +32,33 @@ public class SocketServerShould {
 
   @Test
   void provides_response_body() throws Exception {
+    String content = "<html><body><p>Hello World</p></body></html>";
+    socketServer = new SocketServer(content);
     socketServer.start(4444);
     URL localhost = new URL("http://localhost:4444/");
     URLConnection con = localhost.openConnection();
-    String response = "";
-    try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
-      response = in.lines().collect(Collectors.joining("\n"));
-    }
+    String response = readResponseBody(con);
 
     assertThat(response, containsString("Hello World"));
   }
 
-  @AfterEach
-  void cleanUp() throws Exception {
-    socketServer.stop();
+  @Test
+  void is_not_hardcoding_content_length() throws Exception {
+    String content = "<html><body><p>Hallo Welt</p></body></html>";
+    socketServer = new SocketServer(content);
+    socketServer.start(4444);
+    URL localhost = new URL("http://localhost:4444/");
+    URLConnection con = localhost.openConnection();
+    String response = readResponseBody(con);
+
+    assertThat(response, containsString("Hallo Welt"));
+  }
+
+  private String readResponseBody(URLConnection con) throws IOException {
+    String response = "";
+    try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+      response = in.lines().collect(Collectors.joining("\n"));
+    }
+    return response;
   }
 }
