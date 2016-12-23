9626f668bf7bb176174f8fdb9c847e3a605bd22d
Axel Hodler <derxor@gmail.com>
2016-12-23 09:04:47 +0100

    view will take care of replacing

--- src/main/java/co/hodler/View.java
+++ src/main/java/co/hodler/View.java
@@ -1,7 +1,8 @@
 package co.hodler;
 
 public class View {
-  public String toHTML() {
-    return "<html><body><p>Hello World</p></body></html>";
+  public String toHTML(String greeting) {
+    String template = "<html><body><p>${greeting}</p></body></html>";
+    return template.replace("${greeting}", greeting);
   }
 }
