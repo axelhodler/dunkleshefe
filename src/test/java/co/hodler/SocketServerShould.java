23dd0f1c9fe68aadf1409c40074b06a5052d77e5
Axel Hodler <derxor@gmail.com>
2016-12-25 10:55:11 +0100

    get tests up to speed again

--- src/test/java/co/hodler/SocketServerShould.java
+++ src/test/java/co/hodler/SocketServerShould.java
@@ -1,12 +1,13 @@
 package co.hodler;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.stream.Collectors;
@@ -14,7 +15,6 @@ import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertThat;
 
-@Disabled
 public class SocketServerShould {
 
   private SocketServer socketServer;
@@ -27,16 +27,20 @@ public class SocketServerShould {
   @Test
   void free_the_port_when_stopping() throws Exception {
     socketServer = new SocketServer();
-    socketServer.start(4444);
+    socketServer.start(4444, (s) -> {
+    });
     socketServer.stop();
-    socketServer.start(4444);
+    socketServer.start(4444, (s) -> {
+    });
   }
 
   @Test
   void provides_response_body() throws Exception {
     String content = "<html><body><p>Hello World</p></body></html>";
     socketServer = new SocketServer();
-    socketServer.start(4444);
+    socketServer.start(4444, (s) -> {
+      fakeSocketService(content, s);
+    });
     URL localhost = new URL("http://localhost:4444/");
     URLConnection con = localhost.openConnection();
     String response = readResponseBody(con);
@@ -48,7 +52,9 @@ public class SocketServerShould {
   void is_not_hardcoding_content_length() throws Exception {
     String content = "<html><body><p>Hallo Welt</p></body></html>";
     socketServer = new SocketServer();
-    socketServer.start(4444);
+    socketServer.start(4444, (s) -> {
+      fakeSocketService(content, s);
+    });
     URL localhost = new URL("http://localhost:4444/");
     URLConnection con = localhost.openConnection();
     String response = readResponseBody(con);
@@ -62,5 +68,17 @@ public class SocketServerShould {
       response = in.lines().collect(Collectors.joining("\n"));
     }
     return response;
+  }
+
+  private void fakeSocketService(String content, Socket s) {
+    try (
+      PrintWriter out = new PrintWriter(s.getOutputStream());
+    ) {
+      out.println("HTTP/1.1 200 OK");
+      out.println("Content-Length: " + content.length() + "\n");
+      out.println(content);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
